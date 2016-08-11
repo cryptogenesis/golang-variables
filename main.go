102952274ab8a7aa3ea7032b174cccb7a9a1460e
EVAN ROSE <evan@cryptogenesis.com>
2016-08-11 17:43:44 -0500

    golang variables

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var author = "@cryptogenesis"
+var Version = "0.0.1"
+
+func main() {
+	var greeting string = "Hello world!"
+	var a,b,c int = 1,2,3
+	fmt.Println(greeting, a, b, c)
+
+	var name = "Evan Rose"
+	var d,e,f = 1,2.0,"three"
+	fmt.Println(name, d, e, f)
+
+	course := "Essential Go"
+	x,y,z := 1,2,3
+	fmt.Println(course, x, y, z)
+}
