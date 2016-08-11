fbbe69c16d59768cefefd0137c18fe4c382f3812
EVAN ROSE <evan@cryptogenesis.com>
2016-08-11 17:50:36 -0500

    const/var

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
-var author = "@cryptogenesis"
-var Version = "0.0.1"
+var (
+	author  = "@cryptogenesis"
+	Version = "0.0.1"
+)
+
+const (
+	horse    = "animal"
+	computer = "electronic"
+)
 
 func main() {
 	var greeting string = "Hello world!"
-	var a,b,c int = 1,2,3
+	var a, b, c int = 1, 2, 3
 	fmt.Println(greeting, a, b, c)
 
 	var name = "Evan Rose"
-	var d,e,f = 1,2.0,"three"
+	var d, e, f = 1, 2.0, "three"
 	fmt.Println(name, d, e, f)
 
 	course := "Essential Go"
-	x,y,z := 1,2,3
+	x, y, z := 1, 2, 3
 	fmt.Println(course, x, y, z)
 }
